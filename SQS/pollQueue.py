489ae9838630b01143cf86ab0f8c548c801fdb89
AngelofVerdant <kephareviewsit@gmail.com>
2024-01-07 22:26:06 +0300

    Poll a Queue

--- SQS/pollQueue.py
+++ SQS/pollQueue.py
@@ -1,35 +1,32 @@
-import json
 import boto3
+import time
 
-def lambda_handler(event, context):
-    # Replace 'YOUR_QUEUE_URL' with your SQS queue URL
-    queue_url = 'YOUR_QUEUE_URL'
-
-    # Create SQS client
+def poll_sqs_queue(queue_url):
     sqs = boto3.client('sqs')
 
-    # Receive message from SQS queue
-    response = sqs.receive_message(
-        QueueUrl=queue_url,
-        MaxNumberOfMessages=100,  # Maximum number of messages to receive
-        WaitTimeSeconds=0 # Wait time for messages to arrive (0 to 20 seconds)
-    )
+    while True:
+        # Long polling to receive messages (wait for up to 20 seconds for a message)
+        response = sqs.receive_message(
+            QueueUrl=queue_url,
+            WaitTimeSeconds=20
+        )
 
-    # Check if messages are received
-    if 'Messages' in response:
-        for message in response['Messages']:
-            # Print message body to Lambda logs
-            print(f"Received message: {message['Body']}")
+        if 'Messages' in response:
+            for message in response['Messages']:
+                print(f"Received message: {message['Body']}")
+                
+                # Delete the message from the queue after processing
+                
+                sqs.delete_message(
+                    QueueUrl=queue_url,
+                    ReceiptHandle=message['ReceiptHandle']
+                )
+        else:
+            print("No messages received. Waiting for messages...")
+        
+        time.sleep(1)  # Optional: Adjust the time interval for polling
 
-            # Delete the message from the queue so it's not processed again
-            sqs.delete_message(
-                QueueUrl=queue_url,
-                ReceiptHandle=message['ReceiptHandle']
-            )
-    else:
-        print("No messages available in the queue")
+# Replace 'YOUR_QUEUE_URL' with the actual SQS queue URL
+queue_url = 'YOUR_QUEUE_URL_HERE'
 
-    return {
-        'statusCode': 200,
-        'body': json.dumps('Finished processing messages!')
-    }
+poll_sqs_queue(queue_url)
