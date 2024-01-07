23e7e64afed04b4abd15de40f04929aac7f5cb0c
AngelofVerdant <kephareviewsit@gmail.com>
2024-01-07 16:59:29 +0300

    Send Message to SQS Queue

---
+++ SQS/sendToQueue.py
@@ -0,0 +1,23 @@
+import json
+import boto3
+
+def lambda_handler(event, context):
+    # Replace 'YOUR_QUEUE_URL' with your SQS queue URL
+    queue_url = 'YOUR_QUEUE_URL'
+
+    # Message to be sent to SQS queue
+    message_body = 'Your text message content'
+
+    # Create SQS client
+    sqs = boto3.client('sqs')
+
+    # Send message to SQS queue
+    response = sqs.send_message(
+        QueueUrl=queue_url,
+        MessageBody=message_body
+    )
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Message sent to SQS successfully!')
+    }
